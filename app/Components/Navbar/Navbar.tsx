6738a3c0e24332e90dbece5e43b9ac78da633569
tariktunc <tarktunc@hotmail.com>
2024-02-28 23:31:44 +0300

    Update Navbar and layout

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -3,20 +3,32 @@ import Link from "next/link";
 import { Text } from "@radix-ui/themes";
 export default function Home() {
 	return (
-		<nav className="grid grid-cols-2 h-20  bg-orange-900">
-			<div className="flex items-center mx-10 min-w-fit">
-				<Text size={"8"}>tariktunc</Text>
+		<nav className="grid grid-cols-3 h-20  bg-orange-900">
+			<div className="flex flex-col justify-center items-center">
+				<Text size={"6"}>tariktunc</Text>
+				<Text size={"1"}>operated by blakfy</Text>
 			</div>
-			<div className="h-full flex justify-center items-center gap-5 min-w-fit">
+			<div className="h-full flex justify-center items-center gap-5">
 				<Link href="/">
-					<Button>Home</Button>
+					<Button variant="ghost" size={"4"}>
+						Home
+					</Button>
 				</Link>
 				<Link href="/sign-in">
-					<Button>Sign In</Button>
+					<Button variant="ghost" size={"4"}>
+						About
+					</Button>
 				</Link>
 				<Link href="/sign-up">
-					<Button>Sign Up</Button>
+					<Button variant="ghost" size={"4"}>
+						Contact
+					</Button>
 				</Link>
+			</div>
+			<div className="flex justify-center items-center items-center">
+				<Button variant="outline" size={"2"}>
+					Sign Up
+				</Button>
 			</div>
 		</nav>
 	);
