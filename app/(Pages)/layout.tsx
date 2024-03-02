5579c74af69361a9fef78b66e8ed90ae3b8da39e
tariktunc <tarktunc@hotmail.com>
2024-03-03 01:38:18 +0300

    Delete unused pages and components

--- app/(Pages)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "@/app/Components/Navbar/Navbar";
-export default function Home({ children }: { children: React.ReactNode }) {
-	return (
-		<>
-			<Navbar />
-			{children}
-		</>
-	);
-}
