750fe4912c47c12eac79ae6586d81bfddc70ef84
tariktunc <tarktunc@hotmail.com>
2024-03-06 14:37:07 +0300

    Laboratory page design started and edits were made.

--- app/(Pages)/laboratory/page.tsx
+++ app/(Pages)/laboratory/page.tsx
@@ -1,43 +1,78 @@
 "use client";
 import React, { Suspense } from "react";
-import { Container, Card, Flex, Text, Inset, Strong } from "@radix-ui/themes";
+import {
+	Container,
+	Card,
+	Flex,
+	Text,
+	Inset,
+	Strong,
+	Box,
+	Section,
+} from "@radix-ui/themes";
+import Image from "next/image";
+import Link from "next/link";
+import { Url } from "url";
+import { GitHubLogoIcon, Link2Icon } from "@radix-ui/react-icons";
 
+// CardItem
 interface MyComponentProps {
 	title: string;
 	description: string;
-	srcUrl?: string;
-	streamLink?: string;
+	link?: Url | string;
+	github?: Url | string;
+	imageAdress?: string;
 }
-
+// setProjects useState
 interface Project {
 	title: string;
 	description: string;
-	srcUrl: string;
+	link?: Url | string; // Assuming Url is a type you have defined elsewhere
+	github?: Url | string;
+	imageAdress?: string;
 }
 
 const CardItem = (props: MyComponentProps) => {
 	return (
-		<Card size={"2"} style={{ maxWidth: 300, height: 400 }}>
+		<Card size={"2"} className="grid grid-colums-1 ">
+			{/* Image */}
 			<Inset clip="padding-box" side="top" pb="current">
-				<img
-					src={props.srcUrl || "/blakfy-orginal-icon.jpg"}
-					alt="Bold typography"
-					style={{
-						display: "block",
-						objectFit: "cover",
-						width: "100%",
-						height: 200,
-						backgroundColor: "var(--gray-5)",
-					}}
-				/>
+				<Link href={props.link || "/"}>
+					<Image
+						width={500}
+						height={100}
+						src={props.imageAdress || "/blakfy-orginal-icon.jpg"}
+						alt={props.title}
+						className="aspect-video block object-cover bg-gray-200"
+					/>
+				</Link>
 			</Inset>
-			<Text as="p" size="3">
-				<Strong>{props.title}</Strong>{" "}
-				{props.description.substring(0, 140).concat("...")}
+
+			{/* Title */}
+			<Text
+				as="p"
+				size="3"
+				className=" sm:min-h-40 sm:h-48 md:h-40 lg:h-40 xl:h-44"
+			>
+				<Strong>
+					{props.title.length < 20
+						? props.title
+						: props.title.substring(0, 30).concat("...")}
+				</Strong>{" "}
+				<br />
+				{props.description.length < 141
+					? props.description.substring(0, 140)
+					: props.description.substring(0, 140).concat("...")}
 			</Text>
-			<Text as="p" size="3">
-				Stream Link
-			</Text>
+			{/* Stream Link */}
+			<Flex gap={"5"} justify={"end"} align={"center"} className=" h-8">
+				<Link href={props.github as string}>
+					<GitHubLogoIcon className="w-auto h-6" />
+				</Link>
+				<Link href={props.link as string}>
+					<Link2Icon className="w-auto h-6" />
+				</Link>
+			</Flex>
 		</Card>
 	);
 };
@@ -63,7 +98,7 @@ export default function Home() {
 	React.useEffect(() => {
 		const fetchProjects = async () => {
 			try {
-				const response = await fetch("/laboratory.json");
+				const response = await fetch("/Data/laboratory.json");
 				const data = await response.json();
 				setProjects(data.laboratory);
 				setLoading(true);
@@ -77,20 +112,44 @@ export default function Home() {
 	}, []);
 	return (
 		<Container size={"2"}>
-			<Flex align={"start"} wrap={"wrap"} justify={"center"} gap={"5"}>
+			<Section size={"1"}>
+				<Image
+					src="/laboratorywallpaper2.webp"
+					alt="laboratory wallpaper image"
+					width={5000}
+					height={100}
+					className="aspect-auto w-full block object-cover rounded bg-gray-200"
+				/>
+			</Section>
+			<Section size={"3"}>
+				<Text as="p" size={"8"}>
+					Our Laboratory
+				</Text>
+				<br />
+				<Text as="p" size={"5"}>
+					When creativity and technology come together, we have the power
+					to transform our dreams into reality. Explore our projects and
+					see how we can push the boundaries of creativity in the digital
+					world together. Let's inspire each other on this journey and look
+					towards the future with hope.
+				</Text>
+			</Section>
+			<Box className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 lg:gap-6">
 				{loading ? (
 					projects.map((project) => (
 						<CardItem
+							key={project.title}
 							title={project.title}
 							description={project.description}
-							srcUrl={project.srcUrl}
-							streamLink={"https://where-to-watch-blakfy.netlify.app/"}
+							link={project.link || "/"}
+							imageAdress={project.imageAdress}
+							github={project.github}
 						/>
 					))
 				) : (
 					<LoadingCardItem />
 				)}
-			</Flex>
+			</Box>
 		</Container>
 	);
 }
