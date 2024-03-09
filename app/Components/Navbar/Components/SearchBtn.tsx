0d6c880230c1d9b02742aa0b2461d8875a23cd36
Tarık Tunç <tarik.tunc@ogr.iu.edu.tr>
2024-03-09 20:21:19 +0300

    The search bar design has been changed. Couldn't activate it. !

--- app/Components/Navbar/Components/SearchBtn.tsx
+++ app/Components/Navbar/Components/SearchBtn.tsx
@@ -1,12 +1,17 @@
-import { Button, Flex } from "@radix-ui/themes";
+import { Button, Flex, TextField } from "@radix-ui/themes";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 export default function App() {
-	return (
-		<Flex justify={"start"} align={"center"}>
-			<Button asChild variant="ghost">
-				<MagnifyingGlassIcon width={"25"} height={"25"} />
-			</Button>
-		</Flex>
-	);
+  return (
+    <Flex justify={"start"} align={"center"}>
+      <>
+        <TextField.Root>
+          <TextField.Slot>
+            <MagnifyingGlassIcon height="20" width="20" />
+          </TextField.Slot>
+          <TextField.Input placeholder="Search the docs…" />
+        </TextField.Root>
+      </>
+    </Flex>
+  );
 }
